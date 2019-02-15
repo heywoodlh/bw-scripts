50d6f17e3827ff04d19fcd8b4c262ef1e126e33b
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-02-15 16:28:34 -0700

    updated

--- password-rotate-notify.sh
+++ password-rotate-notify.sh
@@ -6,6 +6,6 @@ RECIPIENT="myemail@gmail.com"
 
 $PY_SCRIPT_PATH --days 90 --session_file ~/.bw_session > /tmp/password-rotate.txt
 
-echo "Attached is a copy of entries in Bitwarden that should be rotated" | mail -s "Password Rotation Scheme" "$RECIPIENT" -A /tmp/password-rotate.txt 
+echo "Attached is a copy of entries in Bitwarden that should be rotated" | mail -s "Password Rotation Scheme" "$RECIPIENT" -a /tmp/password-rotate.txt 
 
 rm /tmp/password-rotate.txt
